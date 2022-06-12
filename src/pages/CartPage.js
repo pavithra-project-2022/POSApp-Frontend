8e0e75061043ece0526c13f089c42891e3ee0f36
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-12 17:55:14 +0800

    commited

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -96,7 +96,7 @@ const CartPage = () => {
         userId: JSON.parse(localStorage.getItem("auth"))._id,
       };
       // console.log(newObject);
-      await axios.post("/api/bills/add-bills", newObject);
+      await axios.post("https://pos-application-backend.herokuapp.com/api/bills/add-bills", newObject);
       message.success("Bill Generated");
       navigate("/bills");
     } catch (error) {
